9493b2a6da6a523760852271b0887820061892f4
mm630 <mm630@snu.edu.in>
2015-07-26 00:06:26 +0530

    the

---
+++ app/src/main/java/com/example/manumaheshwari/gcm_final/OTPConfirmInfo.java
@@ -0,0 +1,13 @@
+package com.example.manumaheshwari.gcm_final;
+
+/**
+ * Created by ManuMaheshwari on 04/07/15.
+ */
+public class OTPConfirmInfo {
+
+    public String customer_id;
+    public String otp;
+    public String created_at;
+
+
+}
---
+++ app/src/main/java/com/example/manumaheshwari/gcm_final/UpdatingServer.java
@@ -0,0 +1,123 @@
+package com.example.manumaheshwari.gcm_final;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by ManuMaheshwari on 04/07/15.
+ */
+public class UpdatingServer {
+
+    static OTPConfirmInfo detail;
+
+
+    public UpdatingServer(){
+
+    }
+
+    public static String POST(String urls){
+
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // 1. create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // 2. make POST request to the given URL
+            HttpPost httpPost = new HttpPost(urls);
+
+            String json = "";
+
+            // 3. build jsonObject
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.accumulate("customer_id", detail.customer_id);
+            jsonObject.accumulate("otp", detail.otp);
+            jsonObject.accumulate("created_at", detail.created_at);
+
+            // 4. convert JSONObject to JSON to String
+            json = jsonObject.toString();
+
+            //Log.d("the thing to the server", json);
+            // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+            // ObjectMapper mapper = new ObjectMapper();
+            // json = mapper.writeValueAsString(person);
+
+            // 5. set json to StringEntity
+            StringEntity se = new StringEntity(json);
+
+            // 6. set httpPost Entity
+            httpPost.setEntity(se);
+
+            // 7. Set some headers to inform server about the type of the content
+            httpPost.setHeader(HTTP.CONTENT_TYPE, "application/x-www-form-urlencoded;charset=UTF-8");
+            //httpPost.setHeader("Content-type", "application/json");
+
+            // 8. Execute POST request to the given URL
+            HttpResponse httpResponse = httpclient.execute(httpPost);
+
+            // 9. receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // 10. convert inputstream to string
+            if(inputStream != null) {
+                result = convertInputStreamToString(inputStream);
+                Log.d("here", result+json);
+            }
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        // 11. return result
+        return result;
+    }
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+
+    public void POSTToServer(){
+
+        new HttpAsyncTask().execute("http://128.199.206.145/vigo/v1/otpdetails");
+
+    }
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls ) {
+
+            return POST(urls[0]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d("its over", "data submitted successfully");
+        }
+    }
+
+
+
+}
